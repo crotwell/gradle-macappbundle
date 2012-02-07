3003d3830508404951d656520ba8ecb2c4543033
Philip Crotwell <crotwell@seis.sc.edu>
2012-02-07 11:33:14 -0500

    add hashCode and equals and Serializable

--- src/main/groovy/edu/sc/seis/gradle/macAppBundle/MacAppBundlePluginExtension.groovy
+++ src/main/groovy/edu/sc/seis/gradle/macAppBundle/MacAppBundlePluginExtension.groovy
@@ -5,7 +5,7 @@ import java.io.File;
 import org.gradle.api.Project;
 
 
-class MacAppBundlePluginExtension {
+class MacAppBundlePluginExtension implements Serializable {
 
     /** The command SetFile, usually located in /Devloper/Tools, that sets the magic bit on a .app directory
      * to turn it into a OSX Application.
@@ -43,4 +43,64 @@ class MacAppBundlePluginExtension {
     public void initExtensionDefaults(Project project) {
         jvmVersion = project.targetCompatibility.toString()+"+"
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((creatorCode == null) ? 0 : creatorCode.hashCode());
+        result = prime * result + ((icon == null) ? 0 : icon.hashCode());
+        result = prime * result + ((jvmVersion == null) ? 0 : jvmVersion.hashCode());
+        result = prime * result + ((mainClassName == null) ? 0 : mainClassName.hashCode());
+        result = prime * result + ((outputDir == null) ? 0 : outputDir.hashCode());
+        result = prime * result + ((setFileCmd == null) ? 0 : setFileCmd.hashCode());
+        result = prime * result + (useScreenMenuBar ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.is(obj))
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MacAppBundlePluginExtension other = (MacAppBundlePluginExtension)obj;
+        if (creatorCode == null) {
+            if (other.creatorCode != null)
+                return false;
+        } else if (!creatorCode.equals(other.creatorCode))
+            return false;
+        if (icon == null) {
+            if (other.icon != null)
+                return false;
+        } else if (!icon.equals(other.icon))
+            return false;
+        if (jvmVersion == null) {
+            if (other.jvmVersion != null)
+                return false;
+        } else if (!jvmVersion.equals(other.jvmVersion))
+            return false;
+        if (mainClassName == null) {
+            if (other.mainClassName != null)
+                return false;
+        } else if (!mainClassName.equals(other.mainClassName))
+            return false;
+        if (outputDir == null) {
+            if (other.outputDir != null)
+                return false;
+        } else if (!outputDir.equals(other.outputDir))
+            return false;
+        if (setFileCmd == null) {
+            if (other.setFileCmd != null)
+                return false;
+        } else if (!setFileCmd.equals(other.setFileCmd))
+            return false;
+        if (useScreenMenuBar != other.useScreenMenuBar)
+            return false;
+        return true;
+    }
+    
+    
 }
