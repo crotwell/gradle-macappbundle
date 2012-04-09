e5377925aa03b27ca0db14494eb26da212946e2d
Philip Crotwell <crotwell@seis.sc.edu>
2012-04-09 16:45:48 -0400

    add def to avoid gradle ext warning

--- src/main/groovy/edu/sc/seis/gradle/macAppBundle/CopyJavaStubTask.groovy
+++ src/main/groovy/edu/sc/seis/gradle/macAppBundle/CopyJavaStubTask.groovy
@@ -9,11 +9,11 @@ class CopyJavaStubTask extends DefaultTask {
 
     @TaskAction
     def void writeStub() {
-        dest = project.file("${project.buildDir}/${project.macAppBundle.outputDir}/${project.macAppBundle.appName}.app/Contents/MacOS/JavaApplicationStub")
+        def dest = project.file("${project.buildDir}/${project.macAppBundle.outputDir}/${project.macAppBundle.appName}.app/Contents/MacOS/JavaApplicationStub")
         dest.parentFile.mkdirs()
-        outStream = new BufferedOutputStream(new FileOutputStream(dest))
-        buf = new byte[1024]
-        inStream = this.getClass().getClassLoader().getResourceAsStream("edu/sc/seis/macAppBundle/JavaApplicationStub")
+        def outStream = new BufferedOutputStream(new FileOutputStream(dest))
+        def buf = new byte[1024]
+        def inStream = this.getClass().getClassLoader().getResourceAsStream("edu/sc/seis/macAppBundle/JavaApplicationStub")
         int numRead = inStream.read(buf)
         while (numRead > 0) {
             outStream.write(buf, 0, numRead)
