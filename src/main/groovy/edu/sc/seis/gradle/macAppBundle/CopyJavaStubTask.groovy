68b4883ec4e66cd629db0f3dffd9123e09c74d5d
Philip Crotwell <crotwell@seis.sc.edu>
2015-03-16 10:03:54 -0400

    had trouble with gradle not finding resource in jar, think it was due to daemon caching contents, but not sure. changes mainly for debugging this, likely didn't matter, but leaving in just in case

--- src/main/groovy/edu/sc/seis/gradle/macAppBundle/CopyJavaStubTask.groovy
+++ src/main/groovy/edu/sc/seis/gradle/macAppBundle/CopyJavaStubTask.groovy
@@ -9,12 +9,17 @@ class CopyJavaStubTask extends DefaultTask {
 
     @TaskAction
     def void writeStub() {
-        def dest = project.file("${project.buildDir}/${project.macAppBundle.appOutputDir}/${project.macAppBundle.appName}.app/Contents/MacOS/${project.macAppBundle.bundleExecutable}")
+        def dest = project.file("${->project.buildDir}/${->project.macAppBundle.appOutputDir}/${->project.macAppBundle.appName}.app/Contents/MacOS/${project.macAppBundle.bundleExecutable}")
         dest.parentFile.mkdirs()
         def outStream = new BufferedOutputStream(new FileOutputStream(dest))
         def buf = new byte[1024]
-        def inStream = this.getClass().getClassLoader().getResourceAsStream("edu/sc/seis/macAppBundle/${project.macAppBundle.bundleExecutable}")
-        if (inStream == null) throw new RuntimeException("Can't find resource for ${project.macAppBundle.bundleExecutable} in jar")
+        Class thisClass = edu.sc.seis.gradle.macAppBundle.CopyJavaStubTask.class;
+        def url = thisClass.getClassLoader().getResource("edu/sc/seis/gradle/macAppBundle/${->project.macAppBundle.bundleExecutable}");
+        
+        def JarURLConnection conn = url.openConnection();
+        conn.connect();
+        def inStream = url.openStream();
+        if (inStream == null) throw new RuntimeException("Can't find resource for ${->project.macAppBundle.bundleExecutable} in jar "+url+"  "+thisClass.getName())
         int numRead = inStream.read(buf)
         while (numRead > 0) {
             outStream.write(buf, 0, numRead)
